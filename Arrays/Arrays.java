5b2b76e843a5ce39fee161fb75c3a5339b3c0396
DnN04 <durgeshnarayan2332@gmail.com>
2025-07-07 21:20:39 +0530

    DAY 3 updated,PERMUTATION

--- Arrays/Arrays.java
+++ Arrays/Arrays.java
@@ -110,7 +110,7 @@ class Solution {
         // 4️⃣ Reverse the right part after idx
         reverse(nums, idx + 1, l - 1);
     }
-
+//
     // Helper method to reverse part of the array
     private void reverse(int[] nums, int start, int end) {
         while (start < end) {
