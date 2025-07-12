4702f656aaeac0a82202acc57c6d236fa45b8a8f
DnN04 <durgeshnarayan2332@gmail.com>
2025-07-12 21:34:30 +0530

    DONE

--- Arrays/Arrays.java
+++ Arrays/Arrays.java
@@ -285,6 +285,26 @@ class Solution {
 //done
 
 //PROBLEM 8: https://leetcode.com/problems/rotate-image/description/
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+        List<int[]> result = new ArrayList<>();
+
+        for (int[] interval : intervals) {
+            // If result is empty or no overlap, add the interval
+            if (result.isEmpty() || result.get(result.size() - 1)[1] < interval[0]) {
+                result.add(interval);
+            } else {
+                // Merge with the last interval
+                result.get(result.size() - 1)[1] = Math.max(result.get(result.size() - 1)[1], interval[1]);
+            }
+        }
+
+        return result.toArray(new int[result.size()][]);
+        
+    }
+}
 
 
 
