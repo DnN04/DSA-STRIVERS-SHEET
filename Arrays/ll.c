c4135e63d37c522aa6dfe4d9ad7836b87e432fca
Durgesh Narayan Nayak <dnofficial2005@gmail.com>
2026-04-04 22:41:36 +0530

    Add decodeCiphertext method in Solution class
    Implemented a method to decode a ciphered text using a matrix approach.

--- Arrays/ll.c
+++ Arrays/ll.c
@@ -22,3 +22,44 @@ struct ListNode* dummy = (struct ListNode*)malloc(sizeof(struct ListNode));
     }
 
     return dummy->next;
+
+
+====================
+    class Solution {
+    public String decodeCiphertext(String encodedText, int rows) {
+        if (rows == 0) return "";
+        
+        int n = encodedText.length();
+        int cols = n / rows;
+        
+        char[][] mat = new char[rows][cols];
+        int idx = 0;
+        
+        // fill matrix row-wise
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                mat[i][j] = encodedText.charAt(idx++);
+            }
+        }
+        
+        StringBuilder res = new StringBuilder();
+        
+        // traverse diagonals
+        for (int j = 0; j < cols; j++) {
+            int r = 0, c = j;
+            while (r < rows && c < cols) {
+                res.append(mat[r][c]);
+                r++;
+                c++;
+            }
+        }
+        
+        // remove trailing spaces
+        int end = res.length() - 1;
+        while (end >= 0 && res.charAt(end) == ' ') {
+            end--;
+        }
+        
+        return res.substring(0, end + 1);
+    }
+}
