ef3317a18ea2beef86cff52acb3bf21dd914fb4b
Durgesh Narayan Nayak <dnofficial2005@gmail.com>
2026-04-05 10:56:42 +0530

    Add Solution class to judge circle movement

--- Arrays/Arrays.java
+++ Arrays/Arrays.java
@@ -342,3 +342,20 @@ class Solution {
     }
 }
 //////////////////////////////////////////////////
+
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int up = 0, down = 0, left = 0, right = 0;
+
+        for (char ch : moves.toCharArray()) {
+            if (ch == 'U') up++;
+            else if (ch == 'D') down++;
+            else if (ch == 'L') left++;
+            else right++;
+        }
+
+        return up == down && left == right;
+    }
+}
+
+==============
