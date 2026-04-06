2f0f203358b8b10ef955ad657e1d50cc066d355d
Durgesh Narayan Nayak <dnofficial2005@gmail.com>
2026-04-06 10:53:55 +0530

    Update Arrays.java

--- Arrays/Arrays.java
+++ Arrays/Arrays.java
@@ -433,3 +433,41 @@ class Solution {
 }
 
 ///////////
+import java.util.*;
+
+class Solution {
+    public int robotSim(int[] commands, int[][] obstacles) {
+        Set<String> set = new HashSet<>();
+        
+        for (int[] obs : obstacles) {
+            set.add(obs[0] + "," + obs[1]);
+        }
+
+        int[] dx = {0, 1, 0, -1};
+        int[] dy = {1, 0, -1, 0};
+
+        int x = 0, y = 0, dir = 0;
+        int maxDist = 0;
+
+        for (int cmd : commands) {
+            if (cmd == -2) {
+                dir = (dir + 3) % 4;
+            } else if (cmd == -1) {
+                dir = (dir + 1) % 4;
+            } else {
+                for (int i = 0; i < cmd; i++) {
+                    int nx = x + dx[dir];
+                    int ny = y + dy[dir];
+
+                    if (set.contains(nx + "," + ny)) break;
+
+                    x = nx;
+                    y = ny;
+                    maxDist = Math.max(maxDist, x * x + y * y);
+                }
+            }
+        }
+
+        return maxDist;
+    }
+}
