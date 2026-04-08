dcaa500e033e0b08dc39f970f29e1b9478f9be4b
Durgesh Narayan Nayak <dnofficial2005@gmail.com>
2026-04-08 10:11:48 +0530

    Add xorAfterQueries method to process array queries
    Implemented xorAfterQueries method to perform XOR operations on a range of array elements based on queries.

--- Arrays/Arrays.java
+++ Arrays/Arrays.java
@@ -1,3 +1,28 @@
+
+class Solution {
+    public int xorAfterQueries(int[] nums, int[][] queries) {
+        int MOD = 1000000007;
+
+        for (int[] q : queries) {
+            int l = q[0];
+            int r = q[1];
+            int k = q[2];
+            int v = q[3];
+
+            for (int i = l; i <= r; i += k) {
+                long val = (long) nums[i] * v;
+                nums[i] = (int) (val % MOD);
+            }
+        }
+
+        int xor = 0;
+        for (int num : nums) {
+            xor ^= num;
+        }
+
+        return xor;
+    }
+}
 // PROBLEM:1- https://leetcode.com/problems/set-matrix-zeroes/
 class Solution {
     public void setZeroes(int[][] matrix) {
