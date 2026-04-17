34f222486450186d210c17b86c390cd1c82c3a09
Durgesh Narayan Nayak <dnofficial2005@gmail.com>
2026-04-17 22:40:07 +0530

    Update Arrays.java

--- Arrays/Arrays.java
+++ Arrays/Arrays.java
@@ -1,3 +1,32 @@
+import java.util.*;
+
+class Solution {
+
+    public int reverse(int x) {
+        int rev = 0;
+        while (x > 0) {
+            rev = rev * 10 + x % 10;
+            x /= 10;
+        }
+        return rev;
+    }
+
+    public int minMirrorPairDistance(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int minDist = Integer.MAX_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                minDist = Math.min(minDist, i - map.get(nums[i]));
+            }
+
+            int rev = reverse(nums[i]);
+            map.put(rev, i);  // 🔥 store reversed value
+        }
+
+        return minDist == Integer.MAX_VALUE ? -1 : minDist;
+    }
+}
 class Solution {
     public int closestTarget(String[] words, String target, int startIndex) {
         int n = words.length;
