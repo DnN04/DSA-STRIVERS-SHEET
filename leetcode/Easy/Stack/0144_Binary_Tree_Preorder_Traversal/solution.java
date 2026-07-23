c6e623dffb2c707aa7843fb9c37e4430f6dace23
DnN04 <dnofficial2005@gmail.com>
2026-07-23 17:03:54 +0530

    Solved #144 Binary Tree Preorder Traversal (Java)

---
+++ leetcode/Easy/Stack/0144_Binary_Tree_Preorder_Traversal/solution.java
@@ -0,0 +1,14 @@
+
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer>list=new ArrayList<>();
+        helper(root,list);
+        return list;
+    }
+    public void helper(TreeNode node,List<Integer>list){
+        if(node==null)return;
+        list.add(node.val);
+        helper(node.left,list);
+        helper(node.right,list);
+    }
+}
